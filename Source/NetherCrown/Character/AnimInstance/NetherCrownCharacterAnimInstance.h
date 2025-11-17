99c9a36f134584243d27ed83c362e7ccac4240f2
DevKelvinWoo <kevin123197a@gmail.com>
2025-11-17 23:06:13 +0900

    feat(chore) : Create AnimInstance class and push ini
    - Create AnimInstance class and push ini

---
+++ Source/NetherCrown/Character/AnimInstance/NetherCrownCharacterAnimInstance.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NetherCrownCharacterAnimInstance.generated.h"
+
+UCLASS()
+class NETHERCROWN_API UNetherCrownCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
