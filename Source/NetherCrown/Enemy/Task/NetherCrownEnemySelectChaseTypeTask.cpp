e2f9342e25a2728bc2509bc1daa741641bf59f78
DevKelvinWoo <kevin123197a@gmail.com>
2026-04-10 15:51:41 +0900

    feat: Add Enemy Chase Type Task and Skill Component
    - Introduced `UNetherCrownEnemySelectChaseTypeTask` behavior tree task to dynamically assign enemy chase types based on conditions.
    - Added `UNetherCrownEnemySkillComponent` for modular enemy skill handling with tick and initialization support.

---
+++ Source/NetherCrown/Enemy/Task/NetherCrownEnemySelectChaseTypeTask.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NetherCrownEnemySelectChaseTypeTask.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+UNetherCrownEnemySelectChaseTypeTask::UNetherCrownEnemySelectChaseTypeTask()
+{
+	NodeName = TEXT("SelectChaseType");
+	ChaseTypeBlackboardKey.AddEnumFilter(this, GET_MEMBER_NAME_CHECKED(ThisClass, ChaseTypeBlackboardKey), StaticEnum<ENetherCrownEnemyChaseType>());
+}
+
+EBTNodeResult::Type UNetherCrownEnemySelectChaseTypeTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UBlackboardComponent* BlackboardComponent{ OwnerComp.GetBlackboardComponent() };
+	if (!ensureAlways(IsValid(BlackboardComponent)))
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	const ENetherCrownEnemyChaseType ChaseType{ BlackboardComponent->GetValueAsEnum(ChaseTypeBlackboardKey.SelectedKeyName) };
+	if (ChaseType != ENetherCrownEnemyChaseType::None)
+	{
+		return EBTNodeResult::Succeeded;
+	}
+
+	const ENetherCrownEnemyChaseType SelectedChaseType{ FMath::FRand() <= 0.25f ? ENetherCrownEnemyChaseType::DashChase : ENetherCrownEnemyChaseType::RunChase };
+	BlackboardComponent->SetValueAsEnum(ChaseTypeBlackboardKey.SelectedKeyName, static_cast<uint8>(SelectedChaseType));
+
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Source/NetherCrown/Enemy/Task/NetherCrownEnemySelectChaseTypeTask.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "NetherCrownEnemySelectChaseTypeTask.generated.h"
+
+UENUM(BlueprintType)
+enum class ENetherCrownEnemyChaseType : uint8
+{
+	None,
+	DashChase,
+	RunChase
+};
+
+UCLASS()
+class NETHERCROWN_API UNetherCrownEnemySelectChaseTypeTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UNetherCrownEnemySelectChaseTypeTask();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector ChaseTypeBlackboardKey{};
+};
